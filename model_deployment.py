89058edc153c8885ef99f20653684ef631f56036
martru118 <50148537+martru118@users.noreply.github.com>
2023-04-16 16:44:35 -0400

    Remove breed from dog images, finish deployment

--- model_deployment.py
+++ model_deployment.py
@@ -1,3 +1,6 @@
+import warnings
+warnings.filterwarnings("ignore")
+
 import torch
 import torchvision.transforms as transforms
 from PIL import Image
@@ -5,32 +8,18 @@ import os
 import glob
 import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt
-
-# order of algorithm
-# 1. get list of images
-# 2. process all the images
-# 3. classify the images and add the classifications to a list
-# 4. print the list
-
-"""
-To finish this we need to create a directory that contains images of type .jpg
-Also need to include a model in the directory, can't to over github I don't think
-Include the model just below. Test
-"""
-model_path = './models/resnet50_model.pt'
-model = torch.load(model_path)
-
-image_directory = './my_dogs'
+import csv
 
 def getImages(directoryName):
     dir_path = directoryName
     file_pattern = '*.jpg'
     image_list = []
+
     for file_path in glob.glob(os.path.join(dir_path, file_pattern)):
         img = Image.open(file_path)
         img_arr = np.array(img)
         image_list.append(img_arr)
+
     return image_list
 
 def process_image(img):
@@ -39,27 +28,59 @@ def process_image(img):
         transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        ])
+    ])
+
+    # apply transformation to image
     img = data_transforms(img)
     img = torch.unsqueeze(img, dim=0)
     return img
 
-def processed_list():
-    original_list = getImages(image_directory)
+def processed_list(dir):
+    original_list = getImages(dir)
     processed_list = []
+
     for img in original_list:
         processed_list.append(process_image(img))
+
     return processed_list
 
 def classify_images(model, processed_img_list):
     classifications = []
+
+    # evaluate model on image set
     for img in processed_img_list:
         model.eval()
         logits = model(img)
-        classifications.append(torch.argmax(logits, dim=1))
+        classifications.append(torch.argmax(logits, dim=1).item())
+
     return classifications
 
-img_list = processed_list()
-classification = classify_images(model, img_list)
+def from_label(label):
+    breed_labels = {}
 
-print(classification)
+    # get record of breeds
+    with open('./records/breeds_data.csv', mode='r') as breeds:
+        data = csv.reader(breeds)
+        breed_labels = {rows[0]:rows[1] for rows in data}
+
+    return breed_labels.get(label)
+
+
+def main():
+    # get model
+    model_path = './models/resnet50_model.pt'
+    model = torch.load(model_path)
+
+    # get dog images
+    image_directory = './my_dogs'
+    img_list = processed_list(image_directory)
+
+    # perform classification
+    classification = classify_images(model, img_list)
+    for label in classification:
+        result = from_label(str(label))
+        result = result.capitalize().replace('_', ' ')
+        print(result)
+
+if __name__ == "__main__":
+    main()
